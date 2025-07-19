02da0cb11d7a2fa8b69207543acbe1092f757a37
AbhiCool <abhijitnaikgovekar@gmail.com>
2025-07-19 22:08:15 +0530

    coorected app.css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import "./app.css";
+import "./App.css";
 import MovieDetail from "./pages/MovieDetail";
 
 const App = () => {
